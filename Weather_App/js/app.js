ab6d5cc7dd57cad26bfbf88af775a2c47c5bc25e
Magdalena Kazimieruk <mkazimieruk@hotmail.com>
2020-08-27 16:59:32 +0200

    Environment setup

---
+++ Weather_App/js/app.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+console.log(process.env);
+const grashopper_api_key = process.env.GRASHOPPER_API_KEY;
+const openWheather_api_key = process.env.OPENWEATHER_API_KEY;
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const entryPath = "Weather_App";
+const entryFile = "app.js";
+
+module.exports = {
+    entry: `./${entryPath}/js/${entryFile}`,
+    output: {
+        filename: "out.js",
+        path: path.resolve(__dirname, `${entryPath}/build`)
+    },
+    devServer: {
+        contentBase: path.join(__dirname, `${entryPath}`),
+        publicPath: "/build/",
+        compress: true,
+        port: 3001
+    },
+    node: {
+        fs: "empty"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    }
+
+};
